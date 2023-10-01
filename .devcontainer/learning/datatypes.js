44f9bcade3da6b1ddbd58f4c5e7275ab8c4a5bd7
SHREYANSH TIWARI <81490172+shreyanshtiwari02@users.noreply.github.com>
2023-10-01 08:05:27 +0000

    color changer

---
+++ .devcontainer/learning/datatypes.js
@@ -0,0 +1,6 @@
+"use strict"
+// alert(3+3)  can only be used in browser
+
+let ame = "hitesh";
+
+console.log(typeof ame);
--- .devcontainer/learning/test.js
+++ .devcontainer/learning/test.js
@@ -1,1 +1,11 @@
 console.log("shrey")
+const myName = "shreyansh"
+let age = 21
+var city = "delhi" 
+/* 
+dont use var and try to always define variable type 
+*/
+// let hello
+greeting = "hi"
+console.log(greeting)
+console.table([myName, age, city, greeting, hello])
